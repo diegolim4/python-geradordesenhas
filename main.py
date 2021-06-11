053c99d6d0618e35bab1f1858a41da4a40f196a9
Diego <lima.j.diego@gmail.com>
2021-06-11 12:43:59 -0300

    Criado gerador de senhas

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 import random, string
+
+
+# tamanho da senha
+size = int(input('Digite o tamanho da senha: '))
+
+# estrutura da senha que será gerada
+chars = string.ascii_letters + string.digits + '@!#%$&().-_+'
+
+rnd = random.SystemRandom()
+
+print(''.join(rnd.choice(chars) for i in range(size)))
